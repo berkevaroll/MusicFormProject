ba028115e32bc364e4de098a13835afbd78c82ad
berkevaroll <berkevaroll@gmail.com>
2020-11-23 21:20:26 +0300

    All done

--- MusicFormProject/Controllers/HomeController.cs
+++ MusicFormProject/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MusicFormProject.Models;
+using MusicFormProject.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +15,39 @@ namespace MusicFormProject.Controllers
             return View();
         }
 
+        public ActionResult Create()
+        {
+            Music model = new Music();
+
+            return View(model);
+        }
+        [HttpPost]
+        public JsonResult Create(Music resModel)
+        {
+            JsonResultModel jsonResultModel = new JsonResultModel();
+            try
+            {
+                DB_TraCaseEntities1 db = new DB_TraCaseEntities1();
+                Music model = new Music();
+                model.SongName = resModel.SongName;
+                model.AlbumName = resModel.AlbumName;
+                model.Artist = resModel.Artist;
+                model.Duration = resModel.Duration;
+                model.PublishYear = Convert.ToInt32(resModel.PublishYear);
+                db.Music.Add(model);
+                db.SaveChanges();
+
+                jsonResultModel.IsSuccess = true;
+                jsonResultModel.ResMessage = "The Song has been added successfully";
+            }
+            catch (Exception ex)
+            {
+                jsonResultModel.IsSuccess = false;
+                jsonResultModel.ResMessage = "Something went wrong! Please try again.";
+            }
+            return Json(jsonResultModel, JsonRequestBehavior.AllowGet);
+            
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ MusicFormProject/ViewModel/JsonResultModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicFormProject.ViewModel
+{
+    public class JsonResultModel
+    {
+        public bool IsSuccess { get; set; }
+
+        public string ResMessage { get; set; }
+    }
+}
